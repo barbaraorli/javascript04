dc9e225d00dfad4534cbecd2da1b8fdbbc4bb536
barbaraorli <barbaramsfsilva@gmail.com>
2019-11-06 16:28:53 -0300

    atualiza exercicio 03

--- objects/ex3/js/nav.js
+++ objects/ex3/js/nav.js
@@ -2,7 +2,7 @@ class Navbar{
     render() {
         const nav = `<img class="navbar__logo" src="./imgs/logo.png" alt="Logo" />
         <div class="group__input">
-            <input class="input__search" type="search" placeholder="Search" aria-label="Search">
+            <input class="input__search" type="search" onKeyUp="limpar{this.value}" placeholder="Search" aria-label="Search">
             <button class="button__search"> Search </button>
         </div>`
         document.querySelector('.navbar').innerHTML = nav   
