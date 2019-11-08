a7c77d5bfa3a3e91997c1ba3159a42e864e29273
barbaraorli <barbaramsfsilva@gmail.com>
2019-11-08 13:36:37 -0300

    atualiza fetch

--- fetch/ex02/main.js
+++ fetch/ex02/main.js
@@ -1,14 +1,25 @@
 const cepInput = document.getElementById("cep")
 const logradouroInput = document.getElementById("logradouro")
+const complementoInput = document.getElementById("complemento")
+const bairroInput = document.getElementById("bairro")
+const localidadeInput = document.getElementById("localidade")
+const ufInput = document.getElementById("uf")
 //... continuem a pegar os inputs do index.html para preencher depois com o valor da api
 
 cepInput.addEventListener("blur", ()=>{
-    if(cepInput.input){
+    if(cepInput.value){
         fetch(`https://viacep.com.br/ws/${cepInput.value}/json`).then(response => {
             return response.json()
         }).then(json => {
-            ///façam aparecer os valores nos inputs
-
+            logradouroInput.value = json.logradouro
+            complementoInput.value = json.complemento
+            bairroInput.value = json.bairro 
+            localidadeInput.value = json.localidade 
+            ufInput.value = json.uf 
         })
     }
 })
+
+// preencheInputs = objeto => {
+
+// }
